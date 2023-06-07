b7aa88117e11b20940967a77b3a4637b8985e1a1
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2023-06-07 20:44:19 +0300

    clear all, pure redux only, another logic to another folder

--- src/components/ReduxPractice.jsx
+++ src/components/ReduxPractice.jsx
@@ -1,17 +1,11 @@
 import ReduxCounter from './ReduxCounter'
 import ReduxBankBalance from './ReduxBankBalance'
-import ReduxCustomers from './ReduxCustomers'
-import CounterSaga from './CounterSaga'
-import ReduxProductList from './ReduxProductList'
 
 const ReduxPractice = () => {
   return (
     <div>
       <ReduxCounter />
       <ReduxBankBalance />
-      <ReduxCustomers />
-      <CounterSaga />
-      <ReduxProductList />
     </div>
   )
 }
--- src/redux-saga/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects'
-import { countWatcher } from './countSaga'
-import { userWatcher } from './userSaga'
-
-export function* rootWatcher() {
-  yield all([countWatcher(), userWatcher()])
-}
